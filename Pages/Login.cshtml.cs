e4edb9ded86e842ad7e4c8689f99df0d379da307
JJWilson-75 <lehuunhan0612@gmail.com>
2024-11-17 08:41:09 +0700

    Login ver 1, still unfinished

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using SWE30003_Group5_Koala.Data;
 using SWE30003_Group5_Koala.Models;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace SWE30003_Group5_Koala.Pages
 {
@@ -28,7 +29,37 @@ namespace SWE30003_Group5_Koala.Pages
             //Console.WriteLine(userEmail);
             //Console.WriteLine(userPassword); 
 
-            var sth = _context.Users.Where(user => user.Email == userEmail && user.Password == userPassword).ToList();
+            var returnedUser = _context.Users.Where(user => user.Email == userEmail && user.Password == userPassword).ToList();
+
+            if (returnedUser.Any())
+            {
+                var cookieOptions = new CookieOptions
+                {
+                    // Set the secure flag, which Chrome's changes will require for SameSite none.
+                    // Note this will also require you to be running on HTTPS
+                    Secure = true,
+
+                    // Set the cookie to HTTP only which is good practice unless you really do need
+                    // to access it client side in scripts.
+                    HttpOnly = true,
+
+                    // Add the SameSite attribute, this will emit the attribute with a value of none.
+                    // To not emit the attribute at all set the SameSite property to SameSiteMode.Unspecified.
+                    SameSite = SameSiteMode.Lax,
+
+                    //Change to set when cookie expires
+                    Expires = DateTime.Now.AddDays(30),
+                };
+
+                var userConvertJson = JsonSerializer.Serialize(returnedUser);
+
+                // Add the cookie to the response cookie collection
+                Response.Cookies.Append("userCookie", userConvertJson, cookieOptions);
+            }
+            else
+            {
+                ViewData["ErrorMessage"] = "Check your information again";
+            }
         }
     }
 }

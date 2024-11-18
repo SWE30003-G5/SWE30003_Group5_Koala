fae206f4c666e63adad927f059d862c19d06c342
baotan1909 <baotan1909@gmail.com>
2024-11-18 12:24:16 +0700

    Update UsersManager's pages ; Hide UsersManager from Staff ; Modify _Layout.cshtml

--- Pages/Manage/UsersManager/Edit.cshtml.cs
+++ Pages/Manage/UsersManager/Edit.cshtml.cs
@@ -13,16 +13,23 @@ namespace SWE30003_Group5_Koala.Pages.Manage.UsersManager
 {
     public class EditModel : PageModel
     {
-        private readonly SWE30003_Group5_Koala.Data.KoalaDbContext _context;
+        private readonly KoalaDbContext _context;
+        private readonly ILogger<EditModel> _logger;
 
-        public EditModel(SWE30003_Group5_Koala.Data.KoalaDbContext context)
+        public EditModel(KoalaDbContext context, ILogger<EditModel> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         [BindProperty]
         public User User { get; set; } = default!;
-
+        public List<SelectListItem> RoleOption { get; set; } = new List<SelectListItem>
+        {
+            new SelectListItem { Text = "Admin", Value = "Admin" },
+            new SelectListItem { Text = "Customer", Value = "Customer" },
+            new SelectListItem { Text = "Staff", Value = "Staff" }
+        };
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
@@ -45,23 +52,32 @@ namespace SWE30003_Group5_Koala.Pages.Manage.UsersManager
         {
             if (!ModelState.IsValid)
             {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    _logger.LogWarning("ModelState Error: {ErrorMessage}", error.ErrorMessage);
+                }
                 return Page();
             }
+
+            _logger.LogInformation($"Attempting to update User with ID {User.ID} and Role {User.Role}");
 
             _context.Attach(User).State = EntityState.Modified;
 
             try
             {
                 await _context.SaveChangesAsync();
+                _logger.LogInformation($"User with ID {User.ID} successfully updated.");
             }
             catch (DbUpdateConcurrencyException)
             {
                 if (!UserExists(User.ID))
                 {
+                    _logger.LogWarning($"User with ID {User.ID} does not exist.");
                     return NotFound();
                 }
                 else
                 {
+                    _logger.LogError("Unexpected concurrency exception while updating the User.");
                     throw;
                 }
             }

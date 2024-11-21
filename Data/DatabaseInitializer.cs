b65f09b9eb75cedc21a799c89ee4744b939ec62a
baotan1909 <baotan1909@gmail.com>
2024-11-21 22:52:08 +0700

    Add seeding for initial tables, users, and menu items data

--- Data/DatabaseInitializer.cs
+++ Data/DatabaseInitializer.cs
@@ -7,6 +7,38 @@ namespace SWE30003_Group5_Koala.Services
     {
         public static void Initialize(KoalaDbContext context)
         {
+            // Seed admin user data
+            if (!context.Users.Any(u => u.Role == "Admin"))
+            {
+                var adminUser = new User
+                {
+                    Name = "John Wick",
+                    Role = "Admin",
+                    Email = "johnwick@gmail.com",
+                    Password = "Admin@1234",
+                    PhoneNumber = "1234567890"
+                };
+                context.Users.Add(adminUser);
+                context.SaveChanges();
+            }
+            // Seed menu items data
+            if (!context.MenuItems.Any())
+            {
+                var menuItems = new[]
+                {
+                    new MenuItem { ID = 1, Name = "Crab Cake", Price = 12, ImageLocation = "Crab_cake.jpg", IsAvailable = false },
+                    new MenuItem { ID = 2, Name = "Risotto", Price = 12, ImageLocation = "Risotto.jpg", IsAvailable = true },
+                    new MenuItem { ID = 3, Name = "Beef Wellington", Price = 25, ImageLocation = "Beef_wellington.jpg", IsAvailable = true },
+                    new MenuItem { ID = 4, Name = "Baked Salmon", Price = 18, ImageLocation = "Baked_salmon.jpg", IsAvailable = true },
+                    new MenuItem { ID = 5, Name = "Fried Chicken", Price = 15, ImageLocation = "Fried_chicken.jpg", IsAvailable = true },
+                    new MenuItem { ID = 6, Name = "Vegetable Soup", Price = 10, ImageLocation = "Vegetable_soup.jpg", IsAvailable = true },
+                    new MenuItem { ID = 7, Name = "Ice Cream", Price = 3, ImageLocation = "Ice_cream.jpg", IsAvailable = true },
+                    new MenuItem { ID = 8, Name = "Coca Cola", Price = 2, ImageLocation = "Coca_cola.jpg", IsAvailable = true }
+                };
+                context.MenuItems.AddRange(menuItems);
+                context.SaveChanges();
+            }
+            // Seed tables data
             if (!context.Tables.Any())
             {
                 var table = new Table
@@ -14,7 +46,6 @@ namespace SWE30003_Group5_Koala.Services
                     Id = 1,
                     IsAvailable = true
                 };
-
                 context.Tables.Add(table);
                 context.SaveChanges();
             }

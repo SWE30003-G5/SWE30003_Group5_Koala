de92ad162a452156ac4d12e1a142c2da2e1eff6b
baotan1909 <baotan1909@gmail.com>
2024-11-21 16:40:37 +0700

    Update homepage

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,60 +14,8 @@ namespace SWE30003_Group5_Koala.Pages
             _logger = logger;
         }
 
-        //The following properties are for test purpose with cookie, can delete later if not needed
-        public int userID { get; set; }
-        public string userEmail { get; set; }
-        public string userName { get; set; }
-        public string userPhoneNumber { get; set; }
-        public string userRole { get; set; }
-        public string userPassword { get; set; }
-
         public void OnGet()
-        {
-            Models.User userCookieClient = new();
-            var cookieJson = Request.Cookies["userCookie"]; //Step 1: get the json file from cookie
-
-            if (cookieJson != null)
-            {
-                // Deserialize the JSON array into a list of users. Because we use ToList in Login.cshtml.cs
-                //Step 2: Deserialize the json of cookie into list
-                var userList = JsonSerializer.Deserialize<List<Models.User>>(cookieJson);
-
-                //Step 3: Check if list has any cookies(objects) in them then user First() to get the first object in the list
-                // Safely check if the list contains at least one user
-                if (userList != null && userList.Any())
-                {
-                    userCookieClient = userList.First();
-
-                    //Step 4: Assign fields from individual objects in list to variables in code, like .Role to know if the role is "Customer", "Staff", "Admin"
-                    // Assign values from the first user in the list
-                    userID = userCookieClient.ID;
-                    userEmail = userCookieClient.Email;
-                    userPhoneNumber = userCookieClient.PhoneNumber;
-                    userName = userCookieClient.Name;
-                    userRole = userCookieClient.Role;
-                    userPassword = userCookieClient.Password;
-                }
-                else
-                {
-                    userID = 0;
-                    userEmail = "Blank";
-                    userPhoneNumber = "Blank";
-                    userName = "Blank";
-                    userRole = "Blank";
-                    userPassword = "Blank";
-                }
-            }
-            else
-            {
-                userID = 0;
-                userEmail = "Blank";
-                userPhoneNumber = "Blank";
-                userName = "Blank";
-                userRole = "Blank";
-                userPassword = "Blank";
-            }
-        }
+        {}
 
         public IActionResult OnPostLogout()
         {
